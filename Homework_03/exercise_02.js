2fb2ffa0853f23cd42b2c1b58c5b7e56230006ee
RedaeMengsteab <redmeha2013@gmail.com>
2019-04-05 16:42:56 -0500

    Delete exercise_02.js

--- Homework_03/exercise_02.js
+++
@@ -1,7 +0,0 @@
-const EventEmitter=require('events');
-class Gym extends EventEmitter{}
-const gym=new Gym();
-gym.on('boom',()=>{
-    console.log("Athlete is working out");
-});
-setInterval(()=>{gym.emit('boom')},1000);
